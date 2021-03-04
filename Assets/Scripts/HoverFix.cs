5d2c541d4d8cbceac02820f6fdb3a54808ae75dd
scooterpsu <3433982+scooterpsu@users.noreply.github.com>
2021-03-03 19:14:48 -0500

    Fix missing hover event hook

---
+++ Assets/Scripts/HoverFix.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class HoverFix : MonoBehaviour
+{
+
+    public void OnHoverEnter(Transform t)
+    {
+        ExecuteEvents.Execute(t.gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerEnterHandler);
+    }
+
+    public void OnHoverExit(Transform t)
+    {
+        ExecuteEvents.Execute(t.gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerExitHandler);
+    }
+
+}
