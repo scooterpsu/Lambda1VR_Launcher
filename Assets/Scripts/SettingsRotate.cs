f1063b297ae6cd5eb6b6da6a3164ebdfadd1650e
scooterpsu <3433982+scooterpsu@users.noreply.github.com>
2021-03-15 23:46:02 -0400

    Add bind to menu button to rotate between game list and settings

---
+++ Assets/Scripts/SettingsRotate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsRotate : MonoBehaviour
+{
+    void Update()
+    {
+        var SettingsButton = OVRInput.GetDown(OVRInput.Button.Start);
+        if (SettingsButton)
+        {
+            RotateCamera();
+        }
+    }
+
+    private int camState = 0;
+    public void RotateCamera()
+    {
+        Vector3 euler = transform.rotation.eulerAngles;
+        if (camState == 0)
+        {
+            euler.y = -90f;
+            camState = 1;
+        }
+        else
+        {
+            euler.y = 0f;
+            camState = 0;
+        }
+        transform.rotation = Quaternion.Euler(euler);
+    }
+}
